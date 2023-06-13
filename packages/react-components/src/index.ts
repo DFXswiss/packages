c7c8de670b24abdd84c4bf4ea72157a5cd0afff8
Krysh <krysh.developer@gmail.com>
2023-06-13 16:01:57 +0200

    fixed build

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -3,7 +3,6 @@ export { CopyButton } from './stories/CopyButton';
 export { default as DfxAssetIcon, AssetIconSize, AssetIconVariant } from './stories/DfxAssetIcon';
 export { default as DfxIcon, IconColor, IconSize, IconVariant } from './stories/DfxIcon';
 export { default as DfxLogo } from './stories/DfxLogo';
-export { default as DfxTitleSection } from './stories/DfxTitleSection';
 export { default as DfxVideoHelpModalContent } from './stories/DfxVideoHelpModalContent';
 export { default as StyledBalance } from './stories/StyledBalance';
 export { default as StyledBalanceSelection } from './stories/StyledBalanceSelection';
