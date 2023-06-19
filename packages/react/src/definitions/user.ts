5d3e384cc82bcf135f674d9189385d2f49056943
Krysh <krysh.developer@gmail.com>
2023-06-19 13:28:59 +0200

    [DEV-1559] react package (#8)
    * [DEV-1559] first version of service + exchange logic
    
    * [DEV-1559] merge latest exchange and services

---
+++ packages/react/src/definitions/user.ts
@@ -0,0 +1,24 @@
+import { KycState, KycStatus } from './kyc';
+
+export const UserUrl = { get: 'user/detail', change: 'user' };
+
+export interface User {
+  mail: string;
+  kycDataComplete: boolean;
+  kycState: KycState;
+  kycStatus: KycStatus;
+  tradingLimit: UserTradingLimit;
+  kycHash: string;
+
+  ref?: string;
+  refFeePercent: number;
+  refCount: number;
+  refVolume: number;
+  refCredit: number;
+  paidRefCredit: number;
+}
+
+export interface UserTradingLimit {
+  limit: number;
+  period: 'Day' | 'Year';
+}
