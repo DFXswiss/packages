b785f189e672fc76c40fb91fbc72ffa6cc191e11
Krysh <krysh.developer@gmail.com>
2023-06-19 13:22:58 +0200

    [DEV-1559] merge latest exchange and services

--- packages/react/src/definitions/buy.ts
+++ packages/react/src/definitions/buy.ts
@@ -16,9 +16,10 @@ export interface Buy {
   remittanceInfo: string;
   minFee: number;
   minVolume: number;
+  minFeeTarget: number;
+  minVolumeTarget: number;
   estimatedAmount: number;
   sepaInstant: boolean;
-  giroCode?: string;
 }
 
 export interface BuyPaymentInfo {
