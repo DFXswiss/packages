6db0f3143843dcfc312df3952ec45fefab588ac8
David May <david.leo.may@gmail.com>
2023-07-06 09:36:29 +0200

    [NO-TASK] Minor adaptions

--- packages/react-components/src/stories/StyledVideoGrid.tsx
+++ packages/react-components/src/stories/StyledVideoGrid.tsx
@@ -1,7 +1,7 @@
 export type VideoSourcesProps = {
   vidSrc: string;
   thumbSrc: string;
-  title: string;
+  title?: string;
 };
 
 export interface StyledVideoGridProps {
@@ -15,7 +15,7 @@ export default function StyledVideoGrid({ videoSources, numCols = 3 }: StyledVid
     <div style={{ gridTemplateColumns: gridFractal.repeat(numCols) }} className="grid gap-6">
       {videoSources.map((video, index) => (
         <div key={index} className="place-self-end">
-          <h3 className="my-3 text-base font-bold">{video.title}</h3>
+          {video.title && <h3 className="my-3 text-base font-bold">{video.title}</h3>}
           <video
             className="rounded-lg border border-white/20"
             src={video.vidSrc}
