7af4cdf93f277666aab6674bacc9eb760d4129b3
David May <david.leo.may@gmail.com>
2023-10-06 12:57:59 +0200

    [NO-TASK] Added link target option

--- packages/react-components/src/stories/StyledLink.tsx
+++ packages/react-components/src/stories/StyledLink.tsx
@@ -1,14 +1,17 @@
+import { HTMLAttributeAnchorTarget } from 'react';
+
 interface StyledLinkProps {
   label: string;
   url?: string;
   dark?: boolean;
+  target?: HTMLAttributeAnchorTarget;
 }
 
-export function StyledLink({ label, url, dark }: StyledLinkProps): JSX.Element {
+export function StyledLink({ label, url, dark, target }: StyledLinkProps): JSX.Element {
   return (
     <a
       className={`${dark ? 'text-dfxGray-800' : 'text-link opacity-30'}`}
-      target="_blank"
+      target={target ?? '_blank'}
       href={url}
       rel="noopener noreferrer"
     >
