f94422a7cfaceef487b3151cf9ca02e4a8d65900
David May <david.leo.may@gmail.com>
2024-05-24 10:34:19 +0200

    [DEV-2556] Added quote error

--- packages/react/src/definitions/transaction.ts
+++ packages/react/src/definitions/transaction.ts
@@ -29,6 +29,7 @@ export enum TransactionError {
   AMOUNT_TOO_HIGH = 'AmountTooHigh',
   BANK_TRANSACTION_MISSING = 'BankTransactionMissing',
   KYC_REQUIRED = 'KycRequired',
+  KYC_DATA_REQUIRED = 'KycDataRequired',
   KYC_REQUIRED_INSTANT = 'KycRequiredInstant',
   LIMIT_EXCEEDED = 'LimitExceeded',
 }
