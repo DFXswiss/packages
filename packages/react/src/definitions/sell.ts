d5fbe3fcc7c5cb6cf9623db70972319b00fbbcd5
lapatric <42653152+lapatric@users.noreply.github.com>
2024-06-25 13:15:34 +0200

    add beneficiary to Sell data type

--- packages/react/src/definitions/sell.ts
+++ packages/react/src/definitions/sell.ts
@@ -27,9 +27,15 @@ export interface Sell {
   exactPrice: boolean;
   estimatedAmount: number;
   currency: Fiat;
+  beneficiary: Beneficary;
   paymentRequest?: string;
   isValid: boolean;
   error?: TransactionError;
+}
+
+export interface Beneficary {
+  iban: string;
+  name: string;
 }
 
 export interface SellPaymentInfo {
