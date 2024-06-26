85705bee6b5fe122ecd2ed4cdebefbfe6afc8845
lapatric <42653152+lapatric@users.noreply.github.com>
2024-06-26 14:34:26 +0200

    add user address switch/delete functions

--- packages/react/src/hooks/user.hook.ts
+++ packages/react/src/hooks/user.hook.ts
@@ -1,11 +1,14 @@
 import { useMemo } from 'react';
 import { Referral, User, UserUrl } from '../definitions/user';
 import { useApi } from './api.hook';
+import { SignIn } from '../definitions/auth';
 
 export interface UserInterface {
   getUser: () => Promise<User | undefined>;
   getRef: () => Promise<Referral | undefined>;
   changeUser: (user?: Partial<User>, userLinkAction?: () => void) => Promise<User | undefined>;
+  changeUserAddress: (address: string) => Promise<SignIn>;
+  deleteUserAddress: () => Promise<void>;
   addDiscountCode: (code: string) => Promise<void>;
 }
 
@@ -34,6 +37,21 @@ export function useUser(): UserInterface {
     });
   }
 
+  async function changeUserAddress(address: string): Promise<SignIn> {
+    return call<SignIn>({
+      url: UserUrl.changeAddress,
+      data: { address },
+      method: 'POST',
+    });
+  }
+
+  async function deleteUserAddress(): Promise<void> {
+    return call({
+      url: UserUrl.delete,
+      method: 'DELETE',
+    });
+  }
+
   async function addDiscountCode(code: string): Promise<void> {
     return call({
       url: `${UserUrl.discountCodes}?code=${code}`,
@@ -41,5 +59,8 @@ export function useUser(): UserInterface {
     });
   }
 
-  return useMemo(() => ({ getUser, getRef, changeUser, addDiscountCode }), [call]);
+  return useMemo(
+    () => ({ getUser, getRef, changeUser, changeUserAddress, deleteUserAddress, addDiscountCode }),
+    [call],
+  );
 }
