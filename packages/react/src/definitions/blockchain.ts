dc9df8609340cb0647363b49d89f658313027749
lapatric <42653152+lapatric@users.noreply.github.com>
2024-07-01 17:22:46 +0200

    add explorer URL getter

--- packages/react/src/definitions/blockchain.ts
+++ packages/react/src/definitions/blockchain.ts
@@ -14,3 +14,24 @@ export enum Blockchain {
   ARWEAVE = 'Arweave',
   CARDANO = 'Cardano',
 }
+
+export function getExplorerUrl(blockchain: Blockchain): string {
+  return BlockchainExplorerUrls[blockchain] ?? '';
+}
+
+const BlockchainExplorerUrls: { [b in Blockchain]: string | undefined } = {
+  [Blockchain.DEFICHAIN]: 'https://defiscan.live',
+  [Blockchain.BITCOIN]: 'https://mempool.space',
+  [Blockchain.LIGHTNING]: undefined,
+  [Blockchain.MONERO]: 'https://xmrscan.org',
+  [Blockchain.ETHEREUM]: 'https://etherscan.io',
+  [Blockchain.BINANCE_SMART_CHAIN]: 'https://bscscan.com',
+  [Blockchain.OPTIMISM]: 'https://optimistic.etherscan.io',
+  [Blockchain.ARBITRUM]: 'https://arbiscan.io',
+  [Blockchain.POLYGON]: 'https://polygonscan.com',
+  [Blockchain.BASE]: 'https://basescan.org',
+  [Blockchain.HAQQ]: 'https://explorer.haqq.network',
+  [Blockchain.LIQUID]: 'https://blockstream.info/liquid',
+  [Blockchain.ARWEAVE]: 'https://arscan.io',
+  [Blockchain.CARDANO]: 'https://cardanoscan.io',
+};
