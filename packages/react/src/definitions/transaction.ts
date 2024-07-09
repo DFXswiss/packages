feb24ddcac52e514f5f1f5b122b7522f0e126314
David May <david.leo.may@gmail.com>
2024-07-09 03:02:13 +0200

    [NO-TASK] Added TX error reasons

--- packages/react/src/definitions/transaction.ts
+++ packages/react/src/definitions/transaction.ts
@@ -72,6 +72,8 @@ export enum TransactionFailureReason {
   INSTANT_PAYMENT = 'InstantPayment',
   FEE_TOO_HIGH = 'FeeTooHigh',
   RECEIVER_REJECTED = 'ReceiverRejected',
+  ASSET_KYC_NEEDED = 'AssetKycNeeded',
+  CHF_ABROAD_NOT_ALLOWED = 'ChfAbroadNotAllowed',
 }
 
 export interface UnassignedTransaction {
