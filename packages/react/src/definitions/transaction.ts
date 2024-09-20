787b6364435409f56b5ec457872fb0c1bb505489
lapatric <42653152+lapatric@users.noreply.github.com>
2024-09-20 18:10:06 +0200

    [DEV-3130] add TX refund hooks

--- packages/react/src/definitions/transaction.ts
+++ packages/react/src/definitions/transaction.ts
@@ -5,12 +5,14 @@ export const TransactionUrl = {
   csv: 'transaction/detail/csv',
   unassigned: 'transaction/unassigned',
   target: 'transaction/target',
+  refund: (id: number) => `transaction/${id}/refund`,
   setTarget: (id: number) => `transaction/${id}/target`,
 };
 
 import { Asset } from './asset';
 import { Blockchain } from './blockchain';
 import { Fees } from './fees';
+import { Fiat } from './fiat';
 import { PriceStep } from './price-step';
 
 export enum FiatPaymentMethod {
@@ -128,6 +130,17 @@ export interface TransactionFilter {
   staking?: boolean;
   ref?: boolean;
   lm?: boolean;
+}
+
+export interface TransactionRefundData {
+  expiryDate: Date;
+  feeAmount: number;
+  refundAmount: number;
+  refundAsset: Asset | Fiat;
+}
+
+export interface TransactionRefundTarget {
+  refundTarget: string;
 }
 
 export type TransactionFilterKey = keyof TransactionFilter;
