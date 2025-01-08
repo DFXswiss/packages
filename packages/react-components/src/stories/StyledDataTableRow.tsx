1e4365f23bdee066b28874929edaec498f3dff82
Patrick <42653152+lapatric@users.noreply.github.com>
2025-01-08 10:22:54 +0100

    [DEV-3384] Edit bank accounts (#87)
    * [DEV-3384] extend BankAccount DTO
    
    * [NO-TASK] add onClick ability to StyledDataTableRow

--- packages/react-components/src/stories/StyledDataTableRow.tsx
+++ packages/react-components/src/stories/StyledDataTableRow.tsx
@@ -10,6 +10,7 @@ interface StyledDataTableRowProps extends PropsWithChildren {
   isLoading?: boolean;
   infoText?: string;
   noPadding?: boolean;
+  onClick?: () => void;
 }
 
 const ALIGN_MAPS: Record<AlignContent, string> = {
@@ -25,6 +26,7 @@ export default function StyledDataTableRow({
   isLoading,
   infoText,
   noPadding,
+  onClick,
 }: StyledDataTableRowProps) {
   const theme = useContext(ThemeContext);
 
@@ -49,10 +51,14 @@ export default function StyledDataTableRow({
     wrapperClasses += ' border-dfxGray-400';
   }
 
+  if (onClick) {
+    wrapperClasses += ' cursor-pointer hover:bg-dfxGray-400';
+  }
+
   rowDataClasses += ALIGN_MAPS[theme.alignContent];
 
   return (
-    <div className={wrapperClasses}>
+    <div className={wrapperClasses} onClick={onClick}>
       <div className="flex">
         {label && (
           <div className={`flex-none ${theme.minWidth ? 'w-48' : ''}`}>
