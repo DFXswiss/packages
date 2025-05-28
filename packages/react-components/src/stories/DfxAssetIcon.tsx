7be5f1d3d155ae2d0a4f2b5ac27d02ed5c57ecf8
David May <david.leo.may@gmail.com>
2025-05-28 15:45:49 +0200

    [DEV-3716] Added SOL icon

--- packages/react-components/src/stories/DfxAssetIcon.tsx
+++ packages/react-components/src/stories/DfxAssetIcon.tsx
@@ -63,6 +63,7 @@ export enum AssetIconVariant {
   nDEPS = 'nDEPS',
   dEURO_PRESALE = 'dEUROPresale',
   DEPS_PRESALE = 'DEPSPresale',
+  SOL = 'SOL',
 
   EUR = 'EUR',
   USD = 'USD',
@@ -141,6 +142,7 @@ const VARIANT_MAPS: Record<AssetIconVariant, (props: BaseAssetIconProps) => Reac
   [AssetIconVariant.nDEPS]: ({ forceColor }) => <DfxAssetIconNDEPS forceColor={forceColor} />,
   [AssetIconVariant.dEURO_PRESALE]: ({ forceColor }) => <DfxAssetIconDEURO forceColor={forceColor} />,
   [AssetIconVariant.DEPS_PRESALE]: ({ forceColor }) => <DfxAssetIconDEPS forceColor={forceColor} />,
+  [AssetIconVariant.SOL]: ({ forceColor }) => <DfxAssetIconSOL forceColor={forceColor} />,
 
   [AssetIconVariant.EUR]: ({ forceColor }) => <DfxAssetIconEUR forceColor={forceColor} />,
   [AssetIconVariant.USD]: ({ forceColor }) => <DfxAssetIconUSD forceColor={forceColor} />,
@@ -5098,6 +5100,70 @@ function DfxAssetIconNDEPS({ forceColor }: BaseAssetIconProps) {
           <path fill="#fff" d="M0 .6h96v96H0z" />
         </clipPath>
       </defs>
+    </svg>
+  );
+}
+
+function DfxAssetIconSOL({ forceColor }: BaseAssetIconProps) {
+  const sizeContext = useContext(SizeContext);
+  return (
+    <svg
+      width={SIZE_MAPS[sizeContext]}
+      height={SIZE_MAPS[sizeContext]}
+      viewBox="0 0 397.7 311.7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <linearGradient
+        id="sol_a"
+        x1={360.879}
+        x2={141.213}
+        y1={351.455}
+        y2={-69.294}
+        gradientTransform="matrix(1 0 0 -1 0 314)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} style={{ stopColor: '#00ffa3' }} />
+        <stop offset={1} style={{ stopColor: '#dc1fff' }} />
+      </linearGradient>
+      <path
+        d="M64.6 237.9c2.4-2.4 5.7-3.8 9.2-3.8h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1l62.7-62.7z"
+        style={{ fill: forceColor ?? 'url(#sol_a)' }}
+      />
+
+      <linearGradient
+        id="sol_b"
+        x1={264.829}
+        x2={45.163}
+        y1={401.601}
+        y2={-19.148}
+        gradientTransform="matrix(1 0 0 -1 0 314)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} style={{ stopColor: '#00ffa3' }} />
+        <stop offset={1} style={{ stopColor: '#dc1fff' }} />
+      </linearGradient>
+      <path
+        d="M64.6 3.8C67.1 1.4 70.4 0 73.8 0h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1L64.6 3.8z"
+        style={{ fill: forceColor ?? 'url(#sol_b)' }}
+      />
+
+      <linearGradient
+        id="sol_c"
+        x1={312.548}
+        x2={92.882}
+        y1={376.688}
+        y2={-44.061}
+        gradientTransform="matrix(1 0 0 -1 0 314)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} style={{ stopColor: '#00ffa3' }} />
+        <stop offset={1} style={{ stopColor: '#dc1fff' }} />
+      </linearGradient>
+      <path
+        d="M333.1 120.1c-2.4-2.4-5.7-3.8-9.2-3.8H6.5c-5.8 0-8.7 7-4.6 11.1l62.7 62.7c2.4 2.4 5.7 3.8 9.2 3.8h317.4c5.8 0 8.7-7 4.6-11.1l-62.7-62.7z"
+        style={{ fill: forceColor ?? 'url(#sol_c)' }}
+      />
     </svg>
   );
 }
