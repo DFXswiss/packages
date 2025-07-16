846ba97dde62cb8f200f75fa765ea5fe9fdc4de2
David May <david.leo.may@gmail.com>
2025-07-16 11:48:43 +0200

    [DEV-4040] Added new KYC step

--- packages/react/src/definitions/kyc.ts
+++ packages/react/src/definitions/kyc.ts
@@ -110,6 +110,7 @@ export enum KycStepName {
   FINANCIAL_DATA = 'FinancialData',
   ADDITIONAL_DOCUMENTS = 'AdditionalDocuments',
   RESIDENCE_PERMIT = 'ResidencePermit',
+  STATUTES = 'Statutes',
   DFX_APPROVAL = 'DfxApproval',
 
   // additional features
